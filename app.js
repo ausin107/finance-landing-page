0b1ee720b2e1b7ad3c530cb4ddb03dbf2f01f4a7
ausin107 <nguyenphuqui396@gmail.com>
2022-07-19 21:40:05 +0700

    add animation and scroll to top button

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-const ele = document.querySelector('.price-input')
-// const priceEle = document.querySelectorAll('.price-value')
+// Change Price Function
+const isCheck = document.querySelector('.price-input')
 const timeEle = document.querySelectorAll('.time-box')
 const priceEle = document.querySelectorAll('.price-box')
-const test = () => {
-  console.log(ele.checked)
+const changePrice = () => {
   timeEle.forEach((item) => {
-    // item.innerHTML = ele.checked == true ? 'Year' : 'Month'
-    ele.checked == true ? item.classList.add('change') : item.classList.remove('change')
+    isCheck.checked == true ? item.classList.add('change') : item.classList.remove('change')
   })
   priceEle.forEach((item) => {
-    // item.innerHTML = ele.checked == true ? 'Year' : 'Month'
-    ele.checked == true ? item.classList.add('change') : item.classList.remove('change')
+    isCheck.checked == true ? item.classList.add('change') : item.classList.remove('change')
   })
 }
-ele.addEventListener('change', test)
+isCheck.addEventListener('change', changePrice)
+//Reveal website onScroll function
+const allItem = document.querySelectorAll('.animation-container')
+const reveal = () => {
+  allItem.forEach((item) => {
+    let windowHeight = window.innerHeight
+    if (item.getBoundingClientRect().top < windowHeight - 150) {
+      item.classList.add('showItem')
+    } else {
+      item.classList.remove('showItem')
+    }
+  })
+}
+
+window.addEventListener('scroll', reveal)
+//Show Back to Top icon Function
+const iconEle = document.querySelector('.back-top-icon ')
+const pointEle = document.querySelector('.item-checkpoint')
+const showIcon = () => {
+  if (pointEle.getBoundingClientRect().top < 0) {
+    iconEle.classList.add('showIcon')
+  } else {
+    iconEle.classList.remove('showIcon')
+  }
+}
+const toTop = () => {
+  window.scrollTo({
+    behavior: `smooth`,
+    left: 0,
+    top: 0,
+  })
+}
+window.addEventListener('scroll', showIcon)
+iconEle.addEventListener('click', toTop)
